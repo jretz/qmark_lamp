985521e09b0e747b0d27271eb14a8b6f7a7b2281
Jimmy Retzlaff <jimmy@retzlaff.com>
2012-05-27 18:03:11 -0700

    do not depend on tornado

--- desktop/qmark_web_service.py
+++ desktop/qmark_web_service.py
@@ -1,86 +1,76 @@
-import tornado.autoreload
-import tornado.ioloop
-import tornado.web
+from BaseHTTPServer import BaseHTTPRequestHandler
+from BaseHTTPServer import HTTPServer
 
 import qmark_serial
 
-_request_handlers = []
+PORT = 8888
+
+commands = None
 
 
-class QuestionMarkHandler(tornado.web.RequestHandler):
-    class __metaclass__(type(tornado.web.RequestHandler)):
-        def __init__(cls, name, bases, class_dict):
-            super(type(cls), cls).__init__(name, bases, class_dict)
-            _request_handlers.append(cls)
+class HTTPRequestHandler(BaseHTTPRequestHandler):
 
-    lamp = None
+    def head(self):
+        command_sequence = self.parse_command_sequence()
+        if command_sequence is None:
+            self.send_response(404)
+        else:
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        return command_sequence
 
-    name = None
-    url = r'/'
+    def do_HEAD(self):
+        self.head()
 
-    def get(self):
-        self.send_command()
-        for handler_class in _request_handlers:
-            if handler_class.name is not None:
-                self.write_link(handler_class.url, handler_class.name)
+    def do_GET(self):
+        command_sequence = self.head()
+        if command_sequence is not None:
+            self.wfile.write('<html><head><title>qmark web service</title></head>')
+            self.wfile.write('<body><p>')
+            for command in command_sequence:
+                command()
+            base_path = self.path.rstrip('/')
+            for name in sorted(commands):
+                self.wfile.write('<a href="%s/%s">%s</a><br>' % (base_path, name, name))
+            self.wfile.write('</p></body></html>')
 
-    def write_link(self, url, name):
-        self.write('<a href="%s">%s</a><br>' % (url, name))
-
-    def send_command(self):
-        pass
-
-
-class LightOffHandler(QuestionMarkHandler):
-    name = 'Light Off'
-    url = r'/light_off'
-
-    def send_command(self):
-        self.lamp.light_off()
-
-
-class LightOnHandler(QuestionMarkHandler):
-    name = 'Light On'
-    url = r'/light_on'
-
-    def send_command(self):
-        self.lamp.light_on()
-
-
-class PlayCoinSoundHandler(QuestionMarkHandler):
-    name = 'Play Coin Sound'
-    url = r'/play_coin_sound'
-
-    def send_command(self):
-        self.lamp.play_coin_sound()
+    def parse_command_sequence(self):
+        path = self.path.lstrip('/')
+        path = path.split('/')
+        if path == ['']:
+            return []
+        try:
+            command_sequence = [commands[name] for name in path]
+        except KeyError:
+            return None
+        return command_sequence
 
 
-class PlayOneUpSoundHandler(QuestionMarkHandler):
-    name = 'Play One Up Sound'
-    url = r'/play_one_up_sound'
+def init_lamp_and_commands():
+    port_name = qmark_serial.guess_port_filename()
+    lamp = qmark_serial.QuestionMarkLamp(port_name)
 
-    def send_command(self):
-        self.lamp.play_one_up_sound()
-
-
-class PauseHandler(QuestionMarkHandler):
-    name = 'Pause'
-    url = r'/pause'
-
-    def send_command(self):
-        self.lamp.pause()
+    global commands
+    commands = dict(
+        light_off=lamp.light_off,
+        light_on=lamp.light_on,
+        play_coin_sound=lamp.play_coin_sound,
+        play_one_up_sound=lamp.play_one_up_sound,
+        pause=lamp.pause
+    )
 
 
 def main():
-    port_name = qmark_serial.guess_port_filename()
-    QuestionMarkHandler.lamp = qmark_serial.QuestionMarkLamp(port_name)
+    init_lamp_and_commands()
 
-    handlers = [tornado.web.url(h.url, h, name=h.name) for h in _request_handlers]
-    application = tornado.web.Application(handlers)
-    application.listen(8888)
-
-    tornado.autoreload.start()
-    tornado.ioloop.IOLoop.instance().start()
+    server_address = ('', PORT)
+    httpd = HTTPServer(server_address, HTTPRequestHandler)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
 
 
 if __name__ == '__main__':
